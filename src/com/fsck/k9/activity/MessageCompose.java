20d0c75eb303596fb3e0e80f221c1321d1e66b8b
Adam Wasserman <imaeses@gmail.com>
2014-03-07 14:23:45 +0100

    Do not HTML format quoted text in replies using plain text

--- src/com/fsck/k9/activity/MessageCompose.java
+++ src/com/fsck/k9/activity/MessageCompose.java
@@ -3588,7 +3588,8 @@ public class MessageCompose extends K9Activity implements OnClickListener,
                 }
             }
 
-            mQuotedText.setCharacters(quoteOriginalTextMessage(mSourceMessage, content, mQuoteStyle));
+            String quotedText = quoteOriginalTextMessage(mSourceMessage, HtmlConverter.htmlToText( content ), mQuoteStyle);
+            mQuotedText.setCharacters(quotedText);
         }
 
         if (showQuotedText) {
